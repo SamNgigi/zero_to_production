d51a33886f4eed8ffe2b794f7d8a51937133eda5
SamNgigi <sam.t.ngigi@gmail.com>
2026-03-27 09:10:48 +0300

    [chore]: Actix branch

--- zero2prod/src/main.rs
+++ zero2prod/src/main.rs
@@ -1,8 +1,45 @@
 use actix_web::{App, HttpServer, middleware::Logger, web};
 use dotenv::dotenv;
+use serde::{Deserialize, Serialize};
+
+// Model
+#[derive(Serialize, Deserialize, Debug)]
+struct BlogPost {
+    id: i32,
+    title: String,
+    content: String,
+    author: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct NewBlogPost {
+    title: String,
+    content: String,
+    author: String,
+}
 
 // INFO: Place holder route
 async fn index_page() -> &'static str {
+    "Hello CRUD API!"
+}
+
+async fn create_blogpost() -> &'static str {
+    "Hello CRUD API!"
+}
+
+async fn read_blogpost() -> &'static str {
+    "Hello CRUD API!"
+}
+
+async fn update_blogpost() -> &'static str {
+    "Hello CRUD API!"
+}
+
+async fn delete_blogpost() -> &'static str {
+    "Hello CRUD API!"
+}
+
+async fn get_all_blogpost() -> &'static str {
     "Hello CRUD API!"
 }
 
