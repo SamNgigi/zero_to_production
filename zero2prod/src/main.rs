4d6b14f7bc0874f2ff064c384e97cdbed76a83d9
SamNgigi <sam.t.ngigi@gmail.com>
2026-04-07 19:41:13 +0300

    [feat]: Adds tracing implementing appropriate intial setup in main and subscriptions.rs

--- zero2prod/src/main.rs
+++ zero2prod/src/main.rs
@@ -1,4 +1,7 @@
 use tokio::net::TcpListener as TokioTcpListener;
+use tracing::subscriber::set_global_default;
+use tracing_log::LogTracer;
+use tracing_subscriber::{EnvFilter, Registry, fmt, layer::SubscriberExt};
 
 use zero2prod::{
     config::{create_pool, get_config},
@@ -7,7 +10,24 @@ use zero2prod::{
 
 #[tokio::main]
 async fn main() {
-    // We panic if we can't read configuration.
+    // INFO: Falling back to info-level or above for all spans
+    LogTracer::init().expect("Failed to set logger");
+    // if the RUST_LOG environment variable has not been set.
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("trace"));
+    let formatting_layer = fmt::layer()
+        .json()
+        .with_current_span(true)
+        .with_span_list(true)
+        .with_target(true)
+        .with_thread_ids(true);
+
+    // INFO: The `with` method is provided by `SubscriberExt`, an extension
+    // trait for `Subscriber` exposed by `tracing_subscriber`
+    let subscriber = Registry::default().with(env_filter).with(formatting_layer);
+    // INFO: `set_global_subscriber` can be used by applications to specify
+    // what subscriber should be used to process spans.
+    set_global_default(subscriber).expect("Failed to set subscriber");
+    // We panic if we can't read configuration
     let config = get_config().expect("Failed to read configuration");
     let connection_pool = create_pool(&config.db)
         .await
--- zero2prod/src/routes/subscriptions.rs
+++ zero2prod/src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
 use axum::{Form, extract::State, http::StatusCode, response::IntoResponse};
 use chrono::Utc;
 use sqlx::PgPool;
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -13,6 +14,25 @@ pub async fn subscribe(
     State(db_pool): State<PgPool>,
     Form(form): Form<FormData>,
 ) -> impl IntoResponse {
+    let request_id = Uuid::now_v7();
+    // INFO: Spans like logs, have an associated level
+    // `info_span` creates a span at the info-level
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber",
+        %request_id,
+        subscriber_email=%form.email,
+        subscriber_username=%form.username
+    );
+
+    // FIX: Using `.enter` in an async function is a recipe for disaster!
+    // Bear with it now, but don't do that at home!
+    // See the following section in the book on 'Instrumenting Futures'
+    let _request_span_guard = request_span.enter();
+
+    // INFO: We do not call `.enter` on the query_span!
+    // `.instrument` takes care of it at the right moments
+    // in the query lifetime
+    let query_span = tracing::info_span!("Saving new subscriber detail in the.");
     match sqlx::query!(
         r#"
             INSERT INTO subscriptions (id, email, username, subscribed_at)
@@ -24,12 +44,24 @@ pub async fn subscribe(
         Utc::now(),
     )
     .execute(&db_pool)
+    // INFO: First we attach the implementation, then we `.await` it.
+    .instrument(query_span)
     .await
     {
-        Ok(_) => StatusCode::OK,
+        Ok(_) => {
+            tracing::info!("request_id {} - New subscriber has been saved.", request_id);
+            StatusCode::OK
+        }
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            tracing::error!(
+                "request_id {} - Failed to execute query: {:?}",
+                request_id,
+                e
+            );
             StatusCode::INTERNAL_SERVER_ERROR
         }
     }
+
+    // INFO:`_request_span_guard` is dropped at the end of `subscribe`
+    // That's when we `exit`
 }
