4c156aa52f045f41e76c0eba0efdbad23573220c
SamNgigi <sam.t.ngigi@gmail.com>
2026-04-07 16:09:27 +0300

    [feat]: Adds tracing span to demonstrate the lifetime of the subscribe fn span

--- zero2prod/src/routes/subscriptions.rs
+++ zero2prod/src/routes/subscriptions.rs
@@ -14,6 +14,30 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>, // Retrieving a connection from App State
 ) -> HttpResponse {
+    let request_id = Uuid::now_v7();
+    // INFO: Spans, like logs, have an associated level
+    // `info_span` creates a span at the info-level
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_username = %form.username
+    );
+
+    // FIX: Using `enter` in an async function is a recipe for disaster!
+    // Bear with it now, but don't do this at home.
+    // See the following section on `Instrumenting Futures`
+    let _request_span_guard = request_span.enter();
+    tracing::info!(
+        "request_id {} - Adding '{}' '{}' as a new subscriber.",
+        request_id,
+        form.email,
+        form.username
+    );
+    tracing::info!(
+        "request_id {} - Saving new subscriber details in the database.",
+        request_id
+    );
     match sqlx::query!(
         r#"
             INSERT INTO subscriptions (id, email, username, subscribed_at)
@@ -27,10 +51,19 @@ pub async fn subscribe(
     .execute(pool.as_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            tracing::info!("request_id {} - New subscriber has been saved.", request_id);
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            tracing::error!(
+                "request_id {} - Failed to execute query: {:?}",
+                request_id,
+                e
+            );
             HttpResponse::InternalServerError().finish()
         }
     }
+    // INFO:`_request_span_guard` is dropped at the end of `subscribe`
+    // That's when we `exit` the span
 }
