1e46a46431edc302fb098b12ec710ab05d8a87e0
SamNgigi <sam.t.ngigi@gmail.com>
2026-04-15 20:52:21 +0300

    [feat]: Adds failing test for SubscriberEmail and SubscriberUsername type.

--- zero2prod/src/domain/subscriber_email.rs
+++ zero2prod/src/domain/subscriber_email.rs
@@ -15,13 +15,24 @@ impl AsRef<str> for SubscriberEmail {
 
 #[cfg(test)]
 mod tests {
+    use crate::domain::SubscriberEmail;
+    use claim::assert_err;
 
     #[test]
-    fn test_empty_email_is_rejected() {}
+    fn test_empty_email_is_rejected() {
+        let email = "".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
 
     #[test]
-    fn test_email_without_domain_is_rejected() {}
+    fn test_email_without_domain_is_rejected() {
+        let email = "lei_yindomain.com".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
 
     #[test]
-    fn test_email_without_subject_is_rejected() {}
+    fn test_email_without_subject_is_rejected() {
+        let email = "@domain.com".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
 }
--- zero2prod/src/domain/subscriber_username.rs
+++ zero2prod/src/domain/subscriber_username.rs
@@ -17,21 +17,44 @@ impl AsRef<str> for SubscriberUsername {
 #[cfg(test)]
 mod tests {
 
-    #[test]
-    fn test_valid_username_is_parsed_successfully() {}
+    use crate::domain::SubscriberUsername;
+    use claim::{assert_err, assert_ok};
 
     #[test]
-    fn test_256_grapheme_long_username_is_valid() {}
+    fn test_valid_username_is_parsed_successfully() {
+        let username = "ley yin loo".to_string();
+        assert_ok!(SubscriberUsername::parse(username));
+    }
 
     #[test]
-    fn test_too_long_username_is_rejected() {}
+    fn test_256_grapheme_long_username_is_valid() {
+        let username = "a̐".repeat(256);
+        assert_err!(SubscriberUsername::parse(username));
+    }
 
     #[test]
-    fn test_empty_username_is_rejected() {}
+    fn test_too_long_username_is_rejected() {
+        let username = "e".repeat(257);
+        assert_err!(SubscriberUsername::parse(username));
+    }
 
     #[test]
-    fn test_only_whitespace_username_is_rejected() {}
+    fn test_empty_username_is_rejected() {
+        let username = "".to_string();
+        assert_err!(SubscriberUsername::parse(username));
+    }
 
     #[test]
-    fn test_username_with_forbidden_chars_is_rejected() {}
+    fn test_only_whitespace_username_is_rejected() {
+        let username = "  ".to_string();
+        assert_err!(SubscriberUsername::parse(username));
+    }
+
+    #[test]
+    fn test_username_with_forbidden_chars_is_rejected() {
+        for username in &['/', '(', ')', '"', '<', '>', '\\', '{', '}'] {
+            let username = username.to_string();
+            assert_err!(SubscriberUsername::parse(username));
+        }
+    }
 }
