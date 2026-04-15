a328afceaea565edcc938920bc59f9b417b26c0f
SamNgigi <sam.t.ngigi@gmail.com>
2026-04-15 16:18:07 +0300

    [feat]: Adds initial tests for email validity and implements SubscriberEmail::parse using validator crate

--- zero2prod/src/domain/subscriber_email.rs
+++ zero2prod/src/domain/subscriber_email.rs
@@ -1,14 +1,50 @@
+use validator::ValidateEmail;
+
 #[derive(Debug)]
 pub struct SubscriberEmail(String);
 
 impl SubscriberEmail {
     pub fn parse(s: String) -> Result<Self, String> {
-        todo!("NOT YET IMPLEMENTED")
+        // INFO: Below call is updated for
+        // newer validator crate, calling it
+        // as a method on `s` as opposed to
+        // a function operating on `&s` in the
+        // original implementation of the book
+        if s.validate_email() {
+            Ok(Self(s))
+        } else {
+            Err(format!("{} is not a valid subscriber email.", s))
+        }
     }
 }
 
 impl AsRef<str> for SubscriberEmail {
     fn as_ref(&self) -> &str {
         &self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::domain::SubscriberEmail;
+    use claims::assert_err;
+
+    #[test]
+    fn empty_email_string_is_rejected() {
+        let email = "".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn email_string_without_at_symbol_is_rejected() {
+        let email = "lei_yindomain.com".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn email_string_without_subject_is_rejected() {
+        let email = "@domain.com".to_string();
+        assert_err!(SubscriberEmail::parse(email));
     }
 }
