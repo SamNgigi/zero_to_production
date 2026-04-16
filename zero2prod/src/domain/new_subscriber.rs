014079685e0de09ce0cb1a56fb6fe51d945c70b1
SamNgigi <sam.t.ngigi@gmail.com>
2026-04-16 13:03:39 +0300

    [feat]: Adds  implementing the FromRequest trait on NewSubscriber as a Learning exercise

--- zero2prod/src/domain/new_subscriber.rs
+++ zero2prod/src/domain/new_subscriber.rs
@@ -1,6 +1,32 @@
-use crate::domain::{subscriber_email::SubscriberEmail, subscriber_username::SubscriberUsername};
+use axum::{
+    Form,
+    extract::{FromRequest, Request},
+    http::StatusCode,
+};
+
+use crate::{
+    domain::{subscriber_email::SubscriberEmail, subscriber_username::SubscriberUsername},
+    routes::FormData,
+};
 
 pub struct NewSubscriber {
     pub username: SubscriberUsername,
     pub email: SubscriberEmail,
+}
+
+impl<S> FromRequest<S> for NewSubscriber
+where
+    S: Send + Sync,
+{
+    type Rejection = (StatusCode, String);
+
+    async fn from_request(req: Request, state: &S) -> Result<Self, Self::Rejection> {
+        // Extract the raw form first
+        let Form(raw_form_data) = Form::<FormData>::from_request(req, state)
+            .await
+            .map_err(|e| (StatusCode::BAD_REQUEST, e.to_string()))?;
+
+        // Apply conversion using TryFrom
+        NewSubscriber::try_from(raw_form_data).map_err(|e| (StatusCode::BAD_REQUEST, e))
+    }
 }
