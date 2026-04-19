9900e09543231778711463f9e8d73cc80a4d5aa7
SamNgigi <sam.t.ngigi@gmail.com>
2026-04-19 07:21:01 +0300

    [feat]: Completes notes on sub-section 'How To Test A REST Client' and adds send_email_fires_request_to_base_url test

--- Justfile
+++ Justfile
@@ -1,2 +1,2 @@
-pull_axum_notes branch_name:
+pull_axum_notes branch_name="chpt7-axum/reject-invalid-subscriber-2":
   git checkout {{branch_name}} -- notebooks
--- zero2prod/src/email_client.rs
+++ zero2prod/src/email_client.rs
@@ -15,13 +15,52 @@ impl EmailClient {
         }
     }
 
-    pub fn send_email(
+    pub async fn send_email(
         &self,
         _recipient_email: SubscriberEmail,
         _subject: &str,
         _html_content: &str,
         _text_content: &str,
     ) -> Result<(), String> {
-        todo!()
+        Ok(()) // No matter the input
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::domain::SubscriberEmail;
+    use crate::email_client::EmailClient;
+
+    use fake::{
+        Fake,
+        faker::{
+            internet::en::SafeEmail,
+            lorem::en::{Paragraph, Sentence},
+        },
+    };
+    use wiremock::{Mock, MockServer, ResponseTemplate, matchers::any};
+
+    #[tokio::test]
+    async fn send_email_fires_request_to_base_url() {
+        // Arrange
+        let mock_server = MockServer::start().await;
+        let sender_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = EmailClient::new(mock_server.uri(), sender_email);
+
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(200))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject: String = Sentence(1..3).fake();
+        let content: String = Paragraph(1..10).fake();
+
+        // Act
+        let _ = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
+            .await;
     }
 }
