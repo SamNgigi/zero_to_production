d4220e23d99ffd3ba91b2f009d496b480ba0b555
SamNgigi <sam.t.ngigi@gmail.com>
2026-04-20 20:36:47 +0300

    [feat]: Adds tests_send_email_fires_request_to_base_url

--- zero2prod/src/email_client.rs
+++ zero2prod/src/email_client.rs
@@ -15,14 +15,14 @@ impl EmailClient {
         }
     }
 
-    pub fn send_email(
+    pub async fn send_email(
         &self,
-        _recipient_email: String,
-        _subject_line: String,
-        _html_content: String,
-        _txt_content: String,
+        _recipient_email: SubscriberEmail,
+        _subject_line: &str,
+        _html_content: &str,
+        _txt_content: &str,
     ) -> Result<(), String> {
-        todo!()
+        Ok(()) // No matter the inputs
     }
 }
 /**
@@ -38,4 +38,36 @@ impl EmailClient {
  *      - Deal appropriately with SecretString and Faker
  * */
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use crate::{domain::SubscriberEmail, email_client::EmailClient};
+
+    use fake::{
+        Fake,
+        faker::{
+            internet::en::SafeEmail,
+            lorem::en::{Paragraph, Sentence},
+        },
+    };
+    use wiremock::{Mock, MockServer, ResponseTemplate, matchers::any};
+
+    #[tokio::test]
+    async fn tests_send_email_fires_request_to_base_url() {
+        let mock_server = MockServer::start().await;
+        let sender_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = EmailClient::new(mock_server.uri(), sender_email);
+
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(200))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+
+        let recipient_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject: String = Sentence(1..3).fake();
+        let content: String = Paragraph(1..10).fake();
+
+        let _ = email_client
+            .send_email(recipient_email, &subject, &content, &content)
+            .await;
+    }
+}
