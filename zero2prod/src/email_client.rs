3d5df9ea822706fca9f79b8e9e7d100cf5d58a66
SamNgigi <sam.t.ngigi@gmail.com>
2026-04-21 19:31:38 +0300

    [feat]: Adds screenshot for 'send_email_times_out_request_takes_too_long' to notes

--- zero2prod/src/email_client.rs
+++ zero2prod/src/email_client.rs
@@ -126,7 +126,9 @@ mod tests {
         let mock_server = MockServer::start().await;
         let email_client = email_client(mock_server.uri());
 
-        let response = ResponseTemplate::new(200).set_delay(std::time::Duration::from_secs(180));
+        let response = ResponseTemplate::new(200)
+            // 3 minutes delay
+            .set_delay(std::time::Duration::from_secs(180));
         Mock::given(any())
             .respond_with(response)
             .expect(1)
