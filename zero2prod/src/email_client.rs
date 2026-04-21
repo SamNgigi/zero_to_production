a049157ffdc8689e782ca8890b59c7cf7bb2ea61
SamNgigi <sam.t.ngigi@gmail.com>
2026-04-21 11:31:29 +0300

    [feat]: Add custom matcher 'SendEmailRequestBodyMatcher' to validate request body has valid json and valid request structure

--- zero2prod/src/email_client.rs
+++ zero2prod/src/email_client.rs
@@ -53,6 +53,7 @@ impl EmailClient {
 }
 
 #[derive(serde::Serialize)]
+#[serde(rename_all = "PascalCase")]
 struct SendEmailRequestBody {
     from: String,
     to: String,
@@ -76,9 +77,31 @@ mod tests {
     };
     use secrecy::SecretString;
     use wiremock::{
-        Mock, MockServer, ResponseTemplate,
+        Match, Mock, MockServer, Request, ResponseTemplate,
         matchers::{header, header_exists, method, path},
     };
+
+    struct SendEmailRequestBodyMatcher;
+
+    impl Match for SendEmailRequestBodyMatcher {
+        fn matches(&self, request: &Request) -> bool {
+            // Try to parse the body as a JSON value
+            let result: Result<serde_json::Value, _> = serde_json::from_slice(&request.body);
+            if let Ok(body) = result {
+                // Check that all the mandatory fields are populated
+                // without inspecting the field values
+                dbg!(&body);
+                body.get("From").is_some()
+                    && body.get("To").is_some()
+                    && body.get("Subject").is_some()
+                    && body.get("HtmlBody").is_some()
+                    && body.get("TextBody").is_some()
+            } else {
+                // If parsing failed, do not match request
+                false
+            }
+        }
+    }
 
     #[tokio::test]
     async fn send_email_fires_request_to_base_url() {
@@ -93,11 +116,6 @@ mod tests {
         );
         /*
          * TODO:
-         * 1. Headers validation commit
-         *      - `header_exits` - token
-         *      - `header` - content type
-         *      - `path` - "/email"
-         *      - `method` - POST
          * 2. Body validation commit
          *      - implement `wiremock::Match trait` on `SendEmailRequestBodyMatcher` unit struct defining
          *        the `matches` method.
@@ -111,6 +129,7 @@ mod tests {
             .and(header("Content-Type", "application/json"))
             .and(path("/email"))
             .and(method("POST"))
+            .and(SendEmailRequestBodyMatcher)
             .respond_with(ResponseTemplate::new(200))
             .expect(1)
             .mount(&mock_server)
