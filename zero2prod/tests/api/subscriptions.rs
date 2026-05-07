79bf0c3d6a77d72254cb879282649366929ac9c5
SamNgigi <sam.t.ngigi@gmail.com>
2026-05-07 12:34:16 +0300

    [test-ref]: Refactor-Step - Adds subscribe_persists_new_subscriber test removing db query and assertions from subscribe_returns_200_for_valid_form_data.

--- zero2prod/tests/api/subscriptions.rs
+++ zero2prod/tests/api/subscriptions.rs
@@ -112,6 +112,32 @@ async fn subscribe_returns_400_when_form_data_is_missing() {
 }
 
 #[tokio::test]
+async fn subscribe_persists_new_subscriber() {
+    // NOTE: Arrange
+    let app = spawn_app().await;
+    let body = "username=lei%20yin&email=lei_yin_loo%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
+    // NOTE: Act
+    app.post_subscriptions(body.into()).await;
+
+    let saved = sqlx::query!("SELECT email, username, status FROM subscriptions",)
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to fetch saved subscriber in test");
+
+    // NOTE: Assert
+    assert_eq!(saved.email, "lei_yin_loo@gmail.com");
+    assert_eq!(saved.username, "lei yin");
+    assert_eq!(saved.status, "pending_confirmation");
+}
+
+#[tokio::test]
 async fn subscribe_returns_200_for_valid_form_data() {
     // Arrange
     let app = spawn_app().await;
@@ -132,14 +158,4 @@ async fn subscribe_returns_200_for_valid_form_data() {
 
     // Assert
     assert_eq!(200, response.status().as_u16());
-
-    // Act
-    let saved = sqlx::query!("SELECT email, username, status FROM subscriptions;")
-        .fetch_one(&app.db_pool)
-        .await
-        .expect("Failed to fetch saved request");
-    // Assert
-    assert_eq!(saved.email, "lei_yin_loo@gmail.com");
-    assert_eq!(saved.username, "lei yin");
-    assert_eq!(saved.status, "pending_confirmation");
 }
