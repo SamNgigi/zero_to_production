89e9f31c4e21ab84d5b1d634289fdee7ecda1427
SamNgigi <sam.t.ngigi@gmail.com>
2026-05-07 08:00:48 +0300

    [feat]: Refactors subscribe and adds 'send_confirmation_email'. Test passing. Completes refactor step.

--- zero2prod/src/routes/subscriptions.rs
+++ zero2prod/src/routes/subscriptions.rs
@@ -52,21 +52,8 @@ pub async fn subscribe(
     if insert_subscriber(&pool, &new_subscriber).await.is_err() {
         return HttpResponse::InternalServerError().finish();
     }
-    let confirmation_link = "https://placeholder-example-domain.com/subscriptions/confirm";
-    if email_client
-        .send_email(
-            new_subscriber.email,
-            "Welcome!",
-            &format!(
-                "Welcome to our newsletter!<br />\
-                Click <a href=\"{}\">here</a> to confirm your subscription.",
-                confirmation_link
-            ),
-            &format!(
-                "Welcome to our newsletter! Visit {} to confirm your subscription.",
-                confirmation_link
-            ),
-        )
+
+    if send_confirmation_email(&email_client, new_subscriber)
         .await
         .is_err()
     {
@@ -74,6 +61,26 @@ pub async fn subscribe(
     }
 
     HttpResponse::Ok().finish()
+}
+
+#[tracing::instrument(name = "Send confirmation email", skip(email_client, new_subscriber))]
+async fn send_confirmation_email(
+    email_client: &EmailClient,
+    new_subscriber: NewSubscriber,
+) -> Result<(), reqwest::Error> {
+    let confirmation_link = "https://placeholder-example-domain.com/subscriptions/confirm";
+    let html_body = format!(
+        "Welcome to our newsletter!<br />\
+        Click <a href=\"{}\">here</a> to confirm your subscription.",
+        confirmation_link
+    );
+    let text_body = format!(
+        "Welcome to our newsletter! Visit {} to confirm your subscription.",
+        confirmation_link
+    );
+    email_client
+        .send_email(new_subscriber.email, "Welcome!", &html_body, &text_body)
+        .await
 }
 
 // INFO: `insert_subscriber` takes care of the database logic and it has no awareness of
