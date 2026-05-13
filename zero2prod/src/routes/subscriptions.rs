c9413f276981ba07219c41aa001743e037592d60
SamNgigi <sam.t.ngigi@gmail.com>
2026-05-13 10:14:44 +0300

    [ref]: Green Test - Updates default new subscriber status to pending_confirmation. Tests passing.

--- zero2prod/src/routes/subscriptions.rs
+++ zero2prod/src/routes/subscriptions.rs
@@ -89,7 +89,7 @@ async fn insert_subscriber(
     sqlx::query!(
         r#"
             INSERT INTO subscriptions (id, email, username, subscribed_at, status)
-            VALUES ($1, $2, $3, $4, 'confirmed')
+            VALUES ($1, $2, $3, $4, 'pending_confirmation')
         "#,
         Uuid::now_v7(),
         new_subscriber.email.as_ref(),
