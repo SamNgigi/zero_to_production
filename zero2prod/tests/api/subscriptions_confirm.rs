c8696120849f34f0f2169f455df1b94be9204907
SamNgigi <sam.t.ngigi@gmail.com>
2026-05-18 20:42:31 +0300

    [test]: Red Test. Adds link_returned_by_subscribe_returns_200_when_called test. Test failing.

--- zero2prod/tests/api/subscriptions_confirm.rs
+++ zero2prod/tests/api/subscriptions_confirm.rs
@@ -1,12 +1,15 @@
 use crate::common::spawn_app;
 
 #[tokio::test]
-async fn confirmation_link_without_subscription_token_rejected_with_400(){
+async fn confirmation_link_without_subscription_token_rejected_with_400() {
+    // NOTE: Arrange
     let app = spawn_app().await;
 
+    // NOTE: Act
     let response = reqwest::get(format!("{}/subscriptions/confirm", app.address))
         .await
         .expect("Failed to execute get subscriptions confirm request");
 
+    // NOTE: Assert
     assert_eq!(response.status().as_u16(), 400);
 }
