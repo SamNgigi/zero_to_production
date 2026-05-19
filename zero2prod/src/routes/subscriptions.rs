439e3985b2a243683c5aa714e074b00f5559ce39
SamNgigi <sam.t.ngigi@gmail.com>
2026-05-19 17:25:32 +0300

    [ref]: Uses a transtion for insert_subscriber and store_token because they need to be executed atomically. Tests still passing.

--- zero2prod/src/routes/subscriptions.rs
+++ zero2prod/src/routes/subscriptions.rs
@@ -1,7 +1,7 @@
 use axum::{Form, extract::State, http::StatusCode, response::IntoResponse};
 use chrono::Utc;
 use rand::{RngExt, distr::Alphanumeric};
-use sqlx::PgPool;
+use sqlx::{Executor, Postgres, Transaction};
 use uuid::Uuid;
 
 use crate::{
@@ -49,17 +49,26 @@ pub async fn subscribe(
         Err(_) => return StatusCode::BAD_REQUEST,
     };
 
-    let subscriber_id = match insert_subscriber(&state.db_pool, &new_subscriber).await {
+    let mut transaction = match state.db_pool.begin().await {
+        Ok(transaction) => transaction,
+        Err(_) => return StatusCode::INTERNAL_SERVER_ERROR,
+    };
+
+    let subscriber_id = match insert_subscriber(&mut transaction, &new_subscriber).await {
         Ok(subscriber_id) => subscriber_id,
         Err(_) => return StatusCode::INTERNAL_SERVER_ERROR,
     };
 
     let subscription_token = generate_subscription_token();
 
-    if store_subscription_token(&state.db_pool, &subscription_token, subscriber_id)
+    if store_subscription_token(&mut transaction, &subscription_token, subscriber_id)
         .await
         .is_err()
     {
+        return StatusCode::INTERNAL_SERVER_ERROR;
+    }
+
+    if transaction.commit().await.is_err() {
         return StatusCode::INTERNAL_SERVER_ERROR;
     }
 
@@ -85,14 +94,14 @@ pub async fn subscribe(
 
 #[tracing::instrument(
     name = "Saving new subscriber details in the database",
-    skip(pool, new_subscriber)
+    skip(transaction, new_subscriber)
 )]
 async fn insert_subscriber(
-    pool: &PgPool,
+    transaction: &mut Transaction<'_, Postgres>,
     new_subscriber: &NewSubscriber,
 ) -> Result<Uuid, sqlx::Error> {
     let subscriber_id = Uuid::now_v7();
-    sqlx::query!(
+    let query = sqlx::query!(
         r#"
             INSERT INTO subscriptions (id, email, username, subscribed_at, status)
             VALUES ($1, $2, $3, $4, 'pending_confirmation')
@@ -101,10 +110,8 @@ async fn insert_subscriber(
         new_subscriber.email.as_ref(),
         new_subscriber.username.as_ref(),
         Utc::now(),
-    )
-    .execute(pool)
-    .await
-    .map_err(|e| {
+    );
+    transaction.execute(query).await.map_err(|e| {
         tracing::error!("Failed to execute query: {:?}", e);
         e
         /* INFO:
@@ -113,7 +120,6 @@ async fn insert_subscriber(
          * We will talk about error handling in depth later!
          * */
     })?;
-
     Ok(subscriber_id)
 }
 
@@ -126,24 +132,23 @@ fn generate_subscription_token() -> String {
 
 #[tracing::instrument(
     name = "Store subscription_token with new subscriber id",
-    skip(db_pool, subscription_token, subscriber_id)
+    skip(transaction, subscription_token, subscriber_id)
 )]
 async fn store_subscription_token(
-    db_pool: &PgPool,
+    transaction: &mut Transaction<'_, Postgres>,
     subscription_token: &str,
     subscriber_id: Uuid,
 ) -> Result<(), sqlx::Error> {
-    sqlx::query!(
+    let query = sqlx::query!(
         r#"
             INSERT INTO subscription_tokens (subscription_token, subscriber_id)
             VALUES ($1, $2)
         "#,
         subscription_token,
         subscriber_id,
-    )
-    .execute(db_pool)
-    .await
-    .map_err(|e| {
+    );
+
+    transaction.execute(query).await.map_err(|e| {
         tracing::error!("Failed to execute store_subscription_token query: {}", e);
         e
     })?;
