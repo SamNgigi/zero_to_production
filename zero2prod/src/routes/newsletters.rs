a65d107097dbde13882b9a2dd9f3413d286fca67
SamNgigi <sam.t.ngigi@gmail.com>
2026-06-12 12:41:36 +0300

    [feat]: Adds BodyData struct and adds it as a JSON arg

--- zero2prod/src/routes/newsletters.rs
+++ zero2prod/src/routes/newsletters.rs
@@ -1,6 +1,18 @@
-use axum::{http::StatusCode, response::IntoResponse};
+use axum::{Json, http::StatusCode, response::IntoResponse};
 
-#[tracing::instrument(name = "Publish newsletter issue")]
-pub async fn publish_newsletter() -> impl IntoResponse {
+#[derive(serde::Deserialize)]
+pub struct BodyData {
+    _title: String,
+    _content: Content,
+}
+
+#[derive(serde::Deserialize)]
+pub struct Content {
+    _html: String,
+    _plain: String,
+}
+
+#[tracing::instrument(name = "Publish newsletter issue", skip(_body))]
+pub async fn publish_newsletter(_body: Json<BodyData>) -> impl IntoResponse {
     StatusCode::OK
 }
