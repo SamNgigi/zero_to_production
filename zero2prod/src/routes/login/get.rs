52541384b0ce2fcb09738ed9c51b2642ebdd17b9
SamNgigi <sam.t.ngigi@gmail.com>
2026-07-18 15:00:58 +0300

    [feat]: Adds expiratin policy to cookie via response.add_removal_cookie.

--- zero2prod/src/routes/login/get.rs
+++ zero2prod/src/routes/login/get.rs
@@ -1,8 +1,4 @@
-use actix_web::{
-    HttpRequest, HttpResponse,
-    cookie::{Cookie, time::Duration},
-    http::header::ContentType,
-};
+use actix_web::{HttpRequest, HttpResponse, cookie::Cookie, http::header::ContentType};
 
 #[tracing::instrument(name = "Login Form")]
 pub async fn login_form(request: HttpRequest) -> HttpResponse {
@@ -10,8 +6,13 @@ pub async fn login_form(request: HttpRequest) -> HttpResponse {
         None => "".into(),
         Some(cookie) => format!("<p><i>{}</i></p>", cookie.value()),
     };
-    HttpResponse::Ok()
+    let mut response = HttpResponse::Ok()
         .content_type(ContentType::html())
-        .cookie(Cookie::build("_flash", "").max_age(Duration::ZERO).finish())
-        .body(format!(include_str!("./login.html"), error_html))
+        .body(format!(include_str!("./login.html"), error_html));
+
+    response
+        .add_removal_cookie(&Cookie::new("_flash", ""))
+        .expect("Failed due to malformed name in cookie header.");
+
+    response
 }
