a73843cbba026f20a6ca5da28fa038d0135894be
SamNgigi <sam.t.ngigi@gmail.com>
2026-07-28 20:14:24 +0300

    [feat]: Green Test. Updates login handler to redirect to '/admin_dashboard'. Test passing.

--- zero2prod/src/routes/login/post.rs
+++ zero2prod/src/routes/login/post.rs
@@ -38,7 +38,7 @@ pub async fn login(
         Ok(user_id) => {
             tracing::Span::current().record("user_id", tracing::field::display(&user_id));
             Ok(HttpResponse::SeeOther()
-                .insert_header((LOCATION, "/home"))
+                .insert_header((LOCATION, "/admin_dashboard"))
                 .finish())
         }
         Err(e) => {
