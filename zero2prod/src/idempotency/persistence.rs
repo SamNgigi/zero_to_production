a0cfc92ad3d3ab4e2b07240d06f9629e815f7b57
SamNgigi <sam.t.ngigi@gmail.com>
2026-09-05 07:56:47 +0300

    [feat]: Adds save_response implementation to src/idempotency/persistence.rs

--- zero2prod/src/idempotency/persistence.rs
+++ zero2prod/src/idempotency/persistence.rs
@@ -1,5 +1,5 @@
 use axum::{
-    body::Body,
+    body::{Body, to_bytes},
     http::{HeaderMap, HeaderName, HeaderValue, StatusCode},
     response::Response,
 };
@@ -8,11 +8,50 @@ use uuid::Uuid;
 
 use super::IdempotencyKey;
 
-#[derive(Debug, sqlx::Type)]
-#[sqlx(type_name = "header_pair")]
-struct HeaderPairRecord {
-    name: String,
-    value: Vec<u8>,
+pub async fn save_response(
+    db_pool: &PgPool,
+    idempotency_key: &IdempotencyKey,
+    user_id: Uuid,
+    response: Response,
+) -> Result<Response, anyhow::Error> {
+    // NOTE: Disassembling response into component parts
+    let (res_head, res_body) = response.into_parts();
+    let status_code = res_head.status.as_u16() as i16;
+    let headers = {
+        let mut h = Vec::with_capacity(res_head.headers.len());
+        for (name, value) in res_head.headers.iter() {
+            let name = name.as_str().to_owned();
+            let value = value.as_bytes().to_owned();
+            h.push(HeaderPairRecord { name, value });
+        }
+        h
+    };
+    let body = to_bytes(res_body, usize::MAX).await?;
+
+    // NOTE: Persisting response component parts to DB
+    sqlx::query!(
+        r#"
+            INSERT INTO idempotency (
+                idempotency_key,
+                user_id,
+                response_status_code,
+                response_headers,
+                response_body,
+                created_at
+            )
+            VALUES ($1, $2, $3, $4, $5, NOW())
+        "#,
+        idempotency_key.as_ref(),
+        user_id,
+        status_code,
+        headers as Vec<HeaderPairRecord>,
+        body.as_ref(),
+    )
+    .execute(db_pool)
+    .await?;
+
+    let response = Response::from_parts(res_head, Body::from(body));
+    Ok(response)
 }
 
 pub async fn get_saved_response(
@@ -20,6 +59,7 @@ pub async fn get_saved_response(
     idempotency_key: &IdempotencyKey,
     user_id: Uuid,
 ) -> Result<Option<Response>, anyhow::Error> {
+    // NOTE: Fetch response from DB.
     let row = sqlx::query!(
         r#"
             SELECT  response_status_code as "response_status_code!",
@@ -36,10 +76,12 @@ pub async fn get_saved_response(
     .fetch_optional(db_pool)
     .await?;
 
+    // NOTE: Return None if no response was found
     let Some(row) = row else {
         return Ok(None);
     };
 
+    // NOTE: Re-assembling response for retreived component parts
     let status_code = StatusCode::from_u16(row.response_status_code.try_into()?)?;
     let headers = {
         let mut h = HeaderMap::with_capacity(row.response_headers.len());
@@ -50,10 +92,17 @@ pub async fn get_saved_response(
         }
         h
     };
-
     let mut response = Response::new(Body::from(row.response_body));
     *response.status_mut() = status_code;
     *response.headers_mut() = headers;
 
+    // NOTE: Returning response
     Ok(Some(response))
+}
+
+#[derive(Debug, sqlx::Type)]
+#[sqlx(type_name = "header_pair")]
+struct HeaderPairRecord {
+    name: String,
+    value: Vec<u8>,
 }
